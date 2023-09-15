f7c89cc22845887bbc9995caf726cbcac9200264
carlosaprost <carlosaprost@gmail.com>
2023-09-14 21:32:29 -0300

    cambio el retor carrera de obstaculos

--- Carrera de obstaculos/carreraDeObstaculos.js
+++ Carrera de obstaculos/carreraDeObstaculos.js
@@ -1,27 +1,19 @@
-function comprobarCarrera(movimientosDelAtleta, pistaDeObstaculos){
-    let pista = pistaDeObstaculos.split('')
-    
-    let movimientos = movimientosDelAtleta.map(m => {
-        if(m === 'RUN'){
-            return '_'
-        }else if(m === 'JUMP'){
-            return '|'
-        }
-    })
-    if(movimientos.length !== pista.length){
-        throw Error
-    }
-    for(let i = 0; i < movimientos.length; i++){
-        if(movimientos[i] === '_' && movimientos[i] !== pista[i]){
-            pista[i] = 'X'
-        }else if (movimientos[i] === '|' && movimientos[i] !== pista[i]){
-            pista[i] = '/'
-        }
-    }
+function comprobarCarrera(movimientosDelAtleta, pistaDeObstaculos) {
+  let pista = pistaDeObstaculos.split("");
 
-    return movimientos.join('') === pista.join('')
+  let movimientos = movimientosDelAtleta.map((m) => {
+    if (m === "RUN") {
+      return "_";
+    } else if (m === "JUMP") {
+      return "|";
+    }
+  });
+
+  return movimientos.join("") === pista.join("");
 }
 
-console.log(comprobarCarrera(['RUN', 'JUMP', 'RUN'], '_|_'))
-console.log(comprobarCarrera(['RUN', 'JUMP', 'RUN'], '_||'))
-console.log(comprobarCarrera(['RUN', 'JUMP', 'RUN', 'JUMP', 'JUMP', 'RUN'], '_|_||_'))
+console.log(comprobarCarrera(["RUN", "JUMP", "RUN"], "_|_"));
+console.log(comprobarCarrera(["RUN", "JUMP", "RUN"], "_||"));
+console.log(
+  comprobarCarrera(["RUN", "JUMP", "RUN", "JUMP", "JUMP", "RUN"], "_|_||_")
+);
